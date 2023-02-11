aa5e5644e1437a74fec231a20343abb2b781f55c
Nya Tyus <nyastyus@gmail.com>
2023-02-11 01:58:39 -0500

    replace html with handlebars

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const path = require("path");
-const PORT = 3001;
-
-// const PORT = process.env.PORT || 3001;
+const routes = require("./controllers/index.js");
+const exphbs = require("express-handlebars");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
+app.engine("handlebars", hbs.engine);
 
-// app.use(express.static(__dirname + "/public"));
+app.set("view engine", "handlebars");
+// Makes express use the routes
 app.use(express.static("public"));
-// GET Route for homepage
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
-
-app.get("/home", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/pages/main.html"))
-);
+app.use(routes);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
